e015e5f92db51482851f44866fb62097194ec8c4
MiBBy-4 <75204033+MiBBy-4@users.noreply.github.com>
2022-06-15 17:38:09 +0300

    refactor: design a navigation

--- src/components/templates/Navigation.jsx
+++ src/components/templates/Navigation.jsx
@@ -2,17 +2,17 @@ import { Navbar, Nav } from 'react-bootstrap';
 
 export default function Navigation() {
   return (
-    <Navbar bg="dark" expand="lg">
-      <Navbar.Toggle aria-controls="basic-navbar-nav" />
-      <Navbar.Collapse id="basic-navbar-nav">
+    <Navbar bg="dark" expand="lg" className="app-navbar">
+      <Navbar.Brand className="app-navbar-brand">EmplDep</Navbar.Brand>
+      <Navbar.Collapse id="basic-navbar-nav" className="app-navbar-items">
         <Nav>
-          <Nav.Link className="d-inline p-2 bg-dark text-white" href="/">
+          <Nav.Link className="d-inline p-2 text-white app-navbar-item" href="/">
             Home
           </Nav.Link>
-          <Nav.Link className="d-inline p-2 bg-dark text-white" href="/employee">
-            Employee
+          <Nav.Link className="d-inline p-2 text-white app-navbar-item" href="/employee">
+            Employees
           </Nav.Link>
-          <Nav.Link className="d-inline p-2 bg-dark text-white" href="/departments">
+          <Nav.Link className="d-inline p-2 text-white app-navbar-item" href="/departments">
             Departments
           </Nav.Link>
         </Nav>
